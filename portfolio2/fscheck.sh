68dd0b77c34d6e20e714f7b4efd21ee177b5c037
shluv1217 <hkluv1009@gmail.com>
2021-09-23 14:03:47 +0900

    Update fscheck.sh

--- portfolio2/fscheck.sh
+++ portfolio2/fscheck.sh
@@ -4,11 +4,12 @@
 
 getprop(){ # declare fucntion named getprop
     wordcount=`wc --word < $1` # set word count of the input file $1 into the variable wordcount
-    filesize=$(du -k "$1" | cut -f 1) # set file size of the input file $1 into the variable filesize
-    date=$(date -r $1 +"%m-%-d-%Y %H:%M:%S") # set last modified date of the input file $1 into the variable date
-    echo "The file $1 contains $wordcount words and is $filesize K in size and was last modified $date" # print wordcount, filesize, last modified date
+    filesize=$(wc -c $1 | awk '{print $1}') #calculate the file size as default(bytes) and save the value as the variable filesize
+    kbsize=$(bc <<<"scale=3; $filesize / 1024") #convert bytes file size to Kbytes file size and save the value as the variable kbsize
+    date=$(date -r $1 +"%d-%m-%Y %H:%M:%S") # set last modified date of the input file $1 into the variable date
+    echo "The file $1 contains $wordcount words and is $kbsize K in size and was last modified $date" # print wordcount, filesize, last modified date
 }
 
-read -p "Plase enther the filename: " filename # prompt the user for the file name
+read -p "Plase enter the filename: " filename # prompt the user for the file name
 getprop $filename # invoke the function named getprop, and set input argument as $filename
 exit 0 # exit program
